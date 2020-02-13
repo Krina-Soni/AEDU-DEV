6cdc056358fa4c5ae6c44ea572b1603c5f3aba3e
Krina-Soni <57283419+Krina-Soni@users.noreply.github.com>
2020-02-13 11:27:57 +0530

    Add files via upload

---
+++ src/test/java/AEDU/Testcases/Basecase.java
@@ -0,0 +1,26 @@
+package AEDU.Testcases;
+import AEDU.constants.CommonVar;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import AEDU.Reports.ReportClass;
+import java.sql.SQLException;
+
+public class Basecase extends ReportClass{
+    public static WebDriver driver;
+    @BeforeMethod
+    public void initialize() throws SQLException
+    {
+        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-debugging-port=9222");
+        options.addArguments("headless");
+
+        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
+        driver = new ChromeDriver();
+        CommonVar common = new CommonVar();
+        driver.get(common.url);
+        driver.manage().window().maximize();
+    }
+}
